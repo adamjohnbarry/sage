9a52f74be70d479046d987db1172a8e8d492575f
brendanmclaughlin <bmclaughlin0407@gmail.com>
2023-12-08 14:03:13 -0800

    margins fixes, bugs

--- app/home/settings/garden-settings.js
+++ app/home/settings/garden-settings.js
@@ -1,7 +1,7 @@
 import { useRouter } from 'expo-router';
 import * as SMS from 'expo-sms';
 import { useContext, useEffect, useState } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, ScrollView, View } from 'react-native';
 import Button from '../../../assets/components/Button';
 import ButtonGroup from '../../../assets/components/ButtonGroup';
 import ContainerScroll from '../../../assets/components/ContainerScroll';
@@ -37,9 +37,9 @@ const GardenSettings = () => {
 	useEffect(() => {
 		setIsChanged(
 			gardenName !== garden?.name ||
-				inviteWord !== garden?.inviteWord ||
-				gardenAddress !== garden?.address ||
-				JSON.stringify(meetingDays) !== JSON.stringify(gardenDaysTimes) // compare stringified objects
+			inviteWord !== garden?.inviteWord ||
+			gardenAddress !== garden?.address ||
+			JSON.stringify(meetingDays) !== JSON.stringify(gardenDaysTimes) // compare stringified objects
 		);
 	}, [gardenName, inviteWord, gardenAddress, meetingDays]);
 
@@ -88,37 +88,39 @@ const GardenSettings = () => {
 	return (
 		<PressOutsideInput>
 			<FormContainer safeArea={safeArea}>
-				<ContainerScroll>
-					<FormGroup label={lang.form.groupMembers.label} spacing={true}>
-						<View style={{ gap: spacing.smSpacing }}>
-							{members.map((member, i) => (
-								<InviteMember key={i} {...member} removeOnPress={() => remove(member)} />
-							))}
-						</View>
-						<TouchableOpacity style={styles.inviteButton} onPress={inviteMembers} color='white'>
-							<Text style={styles.buttonText}>{lang.button.inviteMembers}</Text>
-						</TouchableOpacity>
-					</FormGroup>
-					<FormGroup label={lang.form.gardenName.label} spacing={true}>
-						<FormInputText placeholder={lang.form.gardenName.placeholder} value={gardenName} onChangeText={(text) => setGardenName(text)} />
-					</FormGroup>
-					<FormGroup label={lang.form.inviteWord.label} spacing={true} error={inviteWordError}>
-						<FormInputText placeholder={lang.form.inviteWord.placeholder} value={inviteWord} onChangeText={(text) => setInviteWord(text)} />
-					</FormGroup>
-					<FormGroup label={lang.form.changeLocation.label} spacing={true}>
-						<FormButton
-							icon='map-pin'
-							rightIcon='chevron-right'
-							label={gardenAddress}
-							onPress={() =>
-								router.push({
-									pathname: '/home/settings/change-location',
-									params: { title: lang.settings.changeLocation.title, color: colors.white, hasBackButton: true },
-								})
-							}
-						/>
-					</FormGroup>
-				</ContainerScroll>
+				<ScrollView>
+					<View>
+						<FormGroup label={lang.form.groupMembers.label} spacing={true}>
+							<View style={{ gap: spacing.smSpacing }}>
+								{members.map((member, i) => (
+									<InviteMember key={i} {...member} removeOnPress={() => remove(member)} />
+								))}
+							</View>
+							<TouchableOpacity style={styles.inviteButton} onPress={inviteMembers} color='white'>
+								<Text style={styles.buttonText}>{lang.button.inviteMembers}</Text>
+							</TouchableOpacity>
+						</FormGroup>
+						<FormGroup label={lang.form.gardenName.label} spacing={true}>
+							<FormInputText placeholder={lang.form.gardenName.placeholder} value={gardenName} onChangeText={(text) => setGardenName(text)} />
+						</FormGroup>
+						<FormGroup label={lang.form.inviteWord.label} spacing={true} error={inviteWordError}>
+							<FormInputText placeholder={lang.form.inviteWord.placeholder} value={inviteWord} onChangeText={(text) => setInviteWord(text)} />
+						</FormGroup>
+						<FormGroup label={lang.form.changeLocation.label} spacing={true}>
+							<FormButton
+								icon='map-pin'
+								rightIcon='chevron-right'
+								label={gardenAddress}
+								onPress={() =>
+									router.push({
+										pathname: '/home/settings/change-location',
+										params: { title: lang.settings.changeLocation.title, color: colors.white, hasBackButton: true },
+									})
+								}
+							/>
+						</FormGroup>
+					</View>
+				</ScrollView>
 				<ButtonGroup>
 					<Button text={lang.button.save} onPress={handleSave} />
 				</ButtonGroup>
@@ -134,8 +136,8 @@ const styles = StyleSheet.create({
 		backgroundColor: colors.primary,
 		borderRadius: 100,
 		padding: spacing.mdSpacing,
-		width: '100%',
 		marginTop: spacing.mdSpacing,
+		marginHorizontal: spacing.xlSpacing,
 	},
 	buttonText: {
 		textAlign: 'center',
