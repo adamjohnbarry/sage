0e70741843707e5a2cf3aa51819b5136cf61c76b
brendanmclaughlin <bmclaughlin0407@gmail.com>
2023-12-08 01:12:38 -0800

    .

--- assets/components/Header.js
+++ assets/components/Header.js
@@ -33,7 +33,7 @@ const Header = ({ navigation, route, safeArea }) => {
       )}
       <View style={globalStyles.headerBody}>
         <Text style={globalStyles.h1}>{title}</Text>
-        {/* {gardenAddress && route.name === 'my-garden' && (
+        {gardenAddress && route.name === 'my-garden' && (
           <Text style={[globalStyles.body, { fontSize: fontSizes.body }]}>
             Meets at <Text style={globalStyles.fontBold}>{gardenAddress}</Text>
             {gardenDaysTimes && (
@@ -43,7 +43,7 @@ const Header = ({ navigation, route, safeArea }) => {
               </Text>
             )}
           </Text>
-        )} */}
+        )}
       </View>
     </View>
   );
