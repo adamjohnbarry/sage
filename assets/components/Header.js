d77da1d85ec8fad4e2f3006dad817c1f63a4e40c
Adam Barry <adam211201@gmail.com>
2023-12-08 16:51:38 -0800

    Final yolo.

--- assets/components/Header.js
+++ assets/components/Header.js
@@ -10,12 +10,12 @@ const Header = ({ navigation, route, safeArea }) => {
 	const { lang } = useContext(LangContext);
 	const { garden, gardenDaysTimes } = useUser();
 
-  const formatGardenSchedule = (gardenDaysTimes) => {
-    console.log('GDT: ', gardenDaysTimes);
-    return Object.entries(gardenDaysTimes || {})
-      .map(([day, times]) => `${day} from ${times.join(', ')}`)
-      .join(', ');
-  };
+	const formatGardenSchedule = (gardenDaysTimes) => {
+		console.log('GDT: ', gardenDaysTimes);
+		return Object.entries(gardenDaysTimes || {})
+			.map(([day, times]) => `${day} from ${times.join(', ')}`)
+			.join(', ');
+	};
 
 	// Extract garden details or provide default values
 	const gardenName = garden?.name || 'My Garden';
@@ -25,32 +25,32 @@ const Header = ({ navigation, route, safeArea }) => {
 	let color = route.params?.color || colors.primary;
 	let hasBackButton = route.params?.hasBackButton || false;
 
-  return (
-    <View style={[globalStyles.header, { paddingTop: safeArea.paddingTop + spacing.lgSpacing, backgroundColor: color }]}>
-      {hasBackButton && (
-        <View style={globalStyles.headerNavigation}>
-          <Pressable style={globalStyles.headerBack} onPress={() => navigation.goBack()}>
-            <FontAwesome5 name='long-arrow-alt-left' size={fontSizes.h3} color={colors.black} />
-            <Text style={[globalStyles.fontBold, { fontSize: fontSizes.body }]}>Back</Text>
-          </Pressable>
-        </View>
-      )}
-      <View style={globalStyles.headerBody}>
-        <Text style={globalStyles.h1}>{title}</Text>
-        {gardenAddress && route.name === 'my-garden' && (
-          <Text style={globalStyles.body}>
-            Meets at <Text style={globalStyles.subtitle}>{gardenAddress}</Text>
-            {gardenDaysTimes && (
-              <Text>
-                {' '}
-                on <Text style={globalStyles.subtitle}>{formatGardenSchedule(gardenDaysTimes)}</Text>
-              </Text>
-            )}
-          </Text>
-        )}
-      </View>
-    </View>
-  );
+	return (
+		<View style={[globalStyles.header, { paddingTop: safeArea.paddingTop + spacing.lgSpacing, backgroundColor: color }]}>
+			{hasBackButton && (
+				<View style={globalStyles.headerNavigation}>
+					<Pressable style={globalStyles.headerBack} onPress={() => navigation.goBack()}>
+						<FontAwesome5 name='long-arrow-alt-left' size={fontSizes.h3} color={colors.black} />
+						<Text style={[globalStyles.fontBold, { fontSize: fontSizes.body }]}>{lang.button.back}</Text>
+					</Pressable>
+				</View>
+			)}
+			<View style={globalStyles.headerBody}>
+				<Text style={globalStyles.h1}>{title}</Text>
+				{gardenAddress && route.name === 'my-garden' && (
+					<Text style={globalStyles.body}>
+						Meets at <Text style={globalStyles.subtitle}>{gardenAddress}</Text>
+						{gardenDaysTimes && (
+							<Text>
+								{' '}
+								on <Text style={globalStyles.subtitle}>{formatGardenSchedule(gardenDaysTimes)}</Text>
+							</Text>
+						)}
+					</Text>
+				)}
+			</View>
+		</View>
+	);
 };
 
 export default Header;
